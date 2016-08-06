ee3691767f2817fbede3585cbd7f34fa0cdb86ed
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2016-08-06 15:46:35 -0700

    convert to single quote to double quote for import

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
-import greeter from './ts/greeter';
-import $ = require('jquery');
+import greeter from "./ts/greeter";
+import $ = require("jquery");
 
 $(() => {
     $(document.body).html(greeter("Fagner"));
