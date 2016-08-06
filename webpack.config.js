70da596cf85209c110cc63b255103d30dde7470d
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2016-08-06 16:25:18 -0700

    modified config files

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,17 @@ module.exports = {
     },
     devtool: 'source-map',
     resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
     },
     module: {
         loaders: [
-            { test: /\.ts$/, loader: 'ts-loader' }
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ],
+
+        preLoaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" }
         ]
     }
 }
